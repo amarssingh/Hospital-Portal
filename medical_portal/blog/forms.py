6d35646fbb54a8d936b9218bfa80cc6547b64124
amarssingh <amarsingh.eos@gmail.com>
2025-05-23 02:48:58 +0530

    Delete medical_portal directory

--- medical_portal/blog/forms.py
+++
@@ -1,8 +0,0 @@
-# blog/forms.py
-from django import forms
-from .models import BlogPost
-
-class BlogPostForm(forms.ModelForm):
-    class Meta:
-        model = BlogPost
-        fields = ['title', 'image', 'category', 'summary', 'content', 'is_draft']
--- medical_portal/blog/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('create/', views.create_blog, name='create_blog'),
-    path('my-posts/', views.doctor_posts, name='doctor_posts'),
-    path('all/', views.patient_blog_view, name='patient_blog_view'),
-    path('<int:pk>/', views.blog_detail, name='blog_detail'),
-]
